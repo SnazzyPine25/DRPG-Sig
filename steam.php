934edd80d57fa1368b7d66b27cc88d2b73b92070
Firippu <Firippu@rocketmail.com>
2014-10-05 04:06:21 -0400

    added transparency & cleaned up

--- steam.php
+++ steam.php
@@ -7,87 +7,75 @@ $str_api_key='';
 
 $str_res_dir='./resources/';
 
-$str_sig_file='./signatures/'.$str_input.'.png';
-
 $str_font_reg='tahoma.ttf';
 $str_font_bold='tahomabd.ttf';
 
 $num_font_size=10;
 
-$bGrayScale=TRUE;
+$bool_grayscale=true;
 
 $str_border='border.png';
 $str_bkgnd='bkgnd.png';
 
 $res_image_main=imagecreatefrompng($str_res_dir.$str_bkgnd);
+imagesavealpha($res_image_main,true);
 $res_font_color=imagecolorallocate($res_image_main,0,192,192);
 
 if(empty($str_input) || !is_numeric($str_input) || strlen($str_input)!=17) {
 	$str_error='invalid variable';
 } else {
-	if(file_exists($str_sig_file)) {
-		$bSignatureExists=TRUE;
-		$FileTime=filemtime($str_sig_file);
-		$RealTime=time();
-		$Diff=($RealTime-$FileTime);
-	}
+	$str_url_profile='http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key='.$str_api_key.'&steamids='.$str_input;
 
-	if($bSignatureExists && $Diff<120) {
-		$res_image_main=imagecreatefrompng($str_sig_file);
+	$str_content=file_get_contents($str_url_profile);
+	if($str_content==false) {
+		$str_error='no response from server';
 	} else {
-		$str_url_profile='http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key='.$str_api_key.'&steamids='.$str_input;
+		$str_json=json_decode($str_content,true);
 
-		$str_content=file_get_contents($str_url_profile);
-		if($str_content==false) {
-			$str_error='no response from server';
+		foreach($str_json['response']['players'] as $var_item) {
+			$str_name=$var_item['personaname'];
+			$str_avatar=$var_item['avatarmedium'];
+			$str_game=$var_item['gameextrainfo'];
+			$num_persona=$var_item['personastate'];
+			$num_visible=$var_item['communityvisibilitystate'];
+			$str_steamid=$var_item['steamid'];
+		}
+
+		if(empty($str_steamid)) {
+			$str_error='invalid profile';
 		} else {
-			$str_json=json_decode($str_content,true);
-
-			foreach($str_json['response']['players'] as $var_item) {
-				$str_name=$var_item['personaname'];
-				$str_avatar=$var_item['avatarmedium'];
-				$str_game=$var_item['gameextrainfo'];
-				$num_persona=$var_item['personastate'];
-				$num_visible=$var_item['communityvisibilitystate'];
-				$str_steamid=$var_item['steamid'];
-			}
-
-			if(empty($str_steamid)) {
-				$str_error='invalid profile';
+			if($num_visible!=3) {
+				$arr_privacy=array("","private","friendsonly");
+				$str_state=$arr_privacy[$num_visible];
 			} else {
-				if($num_visible!=3) {
-					$arr_privacy=array("","private","friendsonly");
-					$str_state=$arr_privacy[$num_visible];
+				if(!empty($str_game)) {
+					$str_state='Playing '.$str_game;
+					$bool_grayscale=false;
 				} else {
-					if(!empty($str_game)) {
-						$str_state='Playing '.$str_game;
-						$bGrayScale=FALSE;
-					} else {
-						$arr_state=array("Offline","Online","Busy","Away");
-						$str_state=$arr_state[$num_persona];
-						if($num_persona!=0) {
-							$bGrayScale=FALSE;
-						}
+					$arr_state=array("Offline","Online","Busy","Away");
+					$str_state=$arr_state[$num_persona];
+					if($num_persona!=0) {
+						$bool_grayscale=false;
 					}
 				}
-
-				$res_avatar=imagecreatefromjpeg($str_avatar);
-				imagecopy($res_image_main,$res_avatar,5,5,0,0,64,64);
-
-				$res_border=imagecreatefrompng($str_res_dir.$str_border);
-				imagecopy($res_image_main,$res_border,4,4,0,0,66,66);
-
-				imagettftext($res_image_main,$num_font_size,0,74,22,$res_font_color,$str_res_dir.$str_font_bold,$str_name);
-				imagettftext($res_image_main,$num_font_size,0,74,40,$res_font_color,$str_res_dir.$str_font_reg,$str_state);
 			}
-		}
 
-		if($bGrayScale) {
-			imagefilter($res_image_main,IMG_FILTER_GRAYSCALE);
-		}
+			$res_avatar=imagecreatefromjpeg($str_avatar);
+			imagecopy($res_image_main,$res_avatar,5,5,0,0,64,64);
 
-		imagepng($res_image_main,$str_sig_file,9);
+			$res_border=imagecreatefrompng($str_res_dir.$str_border);
+			imagecopy($res_image_main,$res_border,4,4,0,0,66,66);
+
+			imagettftext($res_image_main,$num_font_size,0,74,22,$res_font_color,$str_res_dir.$str_font_bold,$str_name);
+			imagettftext($res_image_main,$num_font_size,0,74,40,$res_font_color,$str_res_dir.$str_font_reg,$str_state);
+		}
 	}
+
+	if($bool_grayscale) {
+		imagefilter($res_image_main,IMG_FILTER_GRAYSCALE);
+	}
+
+	imagepng($res_image_main,$str_sig_file,9);
 }
 
 if(!empty($str_error)) {
